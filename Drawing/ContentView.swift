9d192f2bc9b270d737386929df38aaf4f70caf32
jjkip <jjlangatjose@gmail.com>
2023-06-22 00:04:43 +0300

    Using ImagePaint  for view backgrounds and also shape strokes

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -132,6 +132,9 @@ struct ContentView: View {
 //                    .border(Image("Example"), width:30)
 //                    .border(ImagePaint(image: Image("Example"), scale: 0.2), width: 30)
                     .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
+                Capsule()
+                    .strokeBorder(ImagePaint(image: Image ("Example"), scale: 0.1), lineWidth: 20)
+                    .frame(width: 300, height: 200)
                 
             }
         }
