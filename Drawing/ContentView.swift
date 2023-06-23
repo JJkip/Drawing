2df263b1d93b7fb4b79dbca2f7ca7c3c0e41f1cf
jjkip <jjlangatjose@gmail.com>
2023-06-23 23:58:48 +0300

    color-cycling view that renders concentric circles in a range of colors

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -81,9 +81,35 @@ struct Flower: Shape {
     }
 }
 
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(color(for: value, brightness: 1), lineWidth: 2)
+            }
+        }
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 struct ContentView: View {
     @State private var petalOffset = -20.0
     @State private var petalWidth = 100.0
+    @State private var colorCycle = 0.0
     
     var body: some View {
         ScrollView {
@@ -136,6 +162,14 @@ struct ContentView: View {
                 Capsule()
                     .strokeBorder(ImagePaint(image: Image ("Example"), scale: 0.1), lineWidth: 20)
                     .frame(width: 300, height: 200)
+                
+                VStack {
+                    ColorCyclingCircle(amount: colorCycle)
+                        .frame(width: 300, height: 300)
+                        
+                    
+                    Slider(value: $colorCycle)
+                }
                 
             }
         }
