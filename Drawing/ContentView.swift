b0a6ab649f8c47ca397b81324708db679376d653
jjkip <jjlangatjose@gmail.com>
2023-06-27 23:58:59 +0300

    Checkerboard drawing without animation

--- Drawing/ContentView.swift
+++ Drawing/ContentView.swift
@@ -7,6 +7,36 @@
 
 import SwiftUI
 
+
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        // figure out how big each row/column needs to be
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+
+        // loop over all rows and columns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    // this square should be colored; add a rectangle here
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+
+        return path
+    }
+}
+
 struct Trapezoid: Shape {
     var insetAmount: Double
     
@@ -144,8 +174,18 @@ struct ContentView: View {
     @State private var colorCycle = 0.0
     @State private var amount = 0.0
     @State private var insetAmount = 50.0
+    @State private var rows = 4
+    @State private var columns = 4
     
     var body: some View {
+        Checkerboard(rows: rows, columns: columns)
+                    .onTapGesture {
+                        withAnimation(.linear(duration: 3)) {
+                            rows = 8
+                            columns = 16
+                        }
+                    }
+        /*
         ScrollView {
             VStack {
                 
@@ -257,6 +297,7 @@ struct ContentView: View {
                     }
             }
         }
+         */
     }
 }
 
